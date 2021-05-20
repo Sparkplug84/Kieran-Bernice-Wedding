972b6e73e9ab6d380a4988ba866b1d27c295cd07
Mark McClean <mcclean_mark_02@hotmail.com>
2021-05-20 21:43:43 +0000

    Created new responsive navbar with slide in menu on small screens

--- kieran-bernice-wedding/src/App.js
+++ kieran-bernice-wedding/src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { auth } from './firebase'
 import './App.css';
 
 // Components
@@ -11,35 +12,48 @@ import login from './pages/login'
 import signup from './pages/signup'
 import posts from './pages/posts'
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Router>
-                        <Switch>
-                            <Route exact path="/" component={homePage}/>
-                            <Route component={sitePages} />
-                        </Switch>
-                </Router>
-            </div>
-        )
-    }
-}
+function App() {
 
-const homePage = () => (
+    const [ user, setUser ] = useState()
+
+    useEffect(() => {
+        auth.onAuthStateChanged((authUser) => {
+            if(authUser) {
+                setUser(authUser)
+            } else {
+                setUser(false)
+            }
+        })
+    }, [])
+    
+    const homePage = () => (
     <Route exact path="/" component={home}/>
 )
 
-const sitePages = () => (
-    <>
-        <Navbar />
-        <div className="container">
-            <Route exact path="/login" component={login}/>
-            <Route exact path="/signup" component={signup}/>
-            <Route exact path="/posts" component={posts}/>
+    const sitePages = () => (
+        <>
+            <Navbar user={user} selected/>
+            <div className="container">
+                <Route exact path="/login" component={login}/>
+                <Route exact path="/signup" component={signup}/>
+                <Route exact path="/posts" component={posts}/>
+            </div>
+        </>
+    )
+
+    return (
+        <div className="App">
+            <Router>
+                    <Switch>
+                        <Route exact path="/" component={homePage}/>
+                        <Route component={sitePages} />
+                    </Switch>
+            </Router>
         </div>
-    </>
-)
+    )
+}
+
+
 
 export default App;
 
--- kieran-bernice-wedding/src/components/Navbar.js
+++ kieran-bernice-wedding/src/components/Navbar.js
@@ -1,23 +1,194 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 import MenuIcon from '@material-ui/icons/Menu';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton'
+import Drawer from '@material-ui/core/Drawer'
+import MenuItem from '@material-ui/core/MenuItem'
+import { makeStyles } from '@material-ui/core/styles';
+import CloseIcon from '@material-ui/icons/Close';
 
-class Navbar extends Component {
-    render() {
+import './Navbar.css'
+
+// Link data for navbar
+const headersData = [
+    {
+      label: "Home",
+      href: "/",
+    },
+  {
+    label: "Posts",
+    href: "/posts",
+  },
+  {
+    label: "Login",
+    href: "/login",
+  },
+  {
+    label: "Sign Up",
+    href: "/signup",
+  },
+];
+
+// styles used in navbar
+const useStyles = makeStyles(() => ({
+        toolbar: {
+            display: "flex",
+            justifyContent: "space-between",
+            ["@media (min-height:400px)"]: { margin: 0, padding: 0 }
+            // width: "100%"
+        },
+        drawerContainer: {
+            padding: "20px 30px",
+            width: "50vw",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            backgroundImage: "linear-gradient(to bottom right, #ff0000, #8500ff)",
+            height: "100%",
+        },
+        menuButton: {
+            fontFamily: "Abel",
+            fontWeight: 500,
+            fontSize: "18px",
+            marginLeft: "38px",
+        },
+        logoContainer: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between"
+        },
+        drawerButtons: {
+            textDecoration: "none",
+            fontFamily: "inherit",
+            color: "#fff",
+            fontSize: "18px",
+            margin: "15px 0"
+        }
+        }));
+
+function Navbar({user, selected}) {
+
+    // Calling the styles used above
+    const { menuButton, toolbar, drawerContainer, logoContainer, drawerButtons } = useStyles();
+    
+    const [state, setState] = useState({
+        mobileView: false,
+        drawerOpen: false,
+    })
+    const { mobileView, drawerOpen } = state;
+    
+    useEffect(() => {
+        const setResponsiveness = () => {
+        return window.innerWidth < 900
+            ? setState((prevState) => ({ ...prevState, mobileView: true }))
+            : setState((prevState) => ({ ...prevState, mobileView: false }));
+        };
+        setResponsiveness();
+        window.addEventListener("resize", () => setResponsiveness());
+    }, []);
+
+    
+
+    const displayDesktop = () => {
         return (
-            <AppBar className="app__header">
-                <Toolbar>
-                    <Button className="app__menulink" component={Link} to="/">Home</Button>
-                    <Button className="app__menulink" component={Link} to="/posts">Posts</Button>
-                    <Button className="app__menulink" component={Link} to="/login">Login</Button>
-                    <Button className="app__menulink" component={Link} to="/signup">Signup</Button>
-                </Toolbar>
-            </AppBar>
-        )
-    }
+        <Toolbar className={toolbar}>
+            <div className={logoContainer}>
+                <img className="app__headerImage" src="https://images.vexels.com/media/users/3/130012/isolated/preview/77f5ebe454a2dfb81433f49a93c153cf-dancing-wedding-couple-by-vexels.png" alt=""/>
+                <h1 className="app__headerlogo">Kieran &amp; Bernice</h1>
+            </div>
+            <div>{getMenuButtons()}</div>
+        </Toolbar>
+        );
+    };
+
+    const displayMobile = () => {
+        const handleDrawerOpen = () =>
+        setState((prevState) => ({ ...prevState, drawerOpen: true }));
+        const handleDrawerClose = () =>
+        setState((prevState) => ({ ...prevState, drawerOpen: false }));
+
+        return (
+        <Toolbar className={toolbar}>
+            <div className={logoContainer}>
+                <img className="app__headerImage" src="https://images.vexels.com/media/users/3/130012/isolated/preview/77f5ebe454a2dfb81433f49a93c153cf-dancing-wedding-couple-by-vexels.png" alt=""/>
+                <h1 className="app__headerlogo">Kieran &amp; Bernice</h1>
+            </div>
+            <IconButton
+            {...{
+                edge: "start",
+                color: "inherit",
+                "aria-label": "menu",
+                "aria-haspopup": "true",
+                onClick: handleDrawerOpen,
+            }}
+            >
+            <MenuIcon  className="navbar__menubutton"/>
+            </IconButton>
+
+            <Drawer
+            {...{
+                anchor: "right",
+                open: drawerOpen,
+                onClose: handleDrawerClose,
+            }}
+            >
+            
+            <CloseIcon onClick={handleDrawerClose} className="navbar__closebutton"/>
+            <div className={drawerContainer}>
+                <h1 className="navbar__drawertitle">Menu</h1>
+                <hr style={{width: "100%", backgroundColor: "#fff", marginBottom: "15px"}}/>
+                {getDrawerChoices()}</div>
+            </Drawer>
+
+        </Toolbar>
+        );
+    };
+
+    const getDrawerChoices = () => {
+        return headersData.map(({ label, href }) => {
+        return (
+            <Button
+            {...{
+                key: label,
+                color: "inherit",
+                to: href,
+                component: Link,
+                className: drawerButtons
+            }}
+            >
+                {label}
+            </Button>
+        );
+        });
+    };
+
+    const getMenuButtons = () => {
+        return headersData.map(({ label, href }) => {
+        return (
+            <Button
+            {...{
+                key: label,
+                color: "inherit",
+                to: href,
+                component: Link,
+                className: menuButton,
+            }}
+            >
+            {label}
+            </Button>
+        );
+        });
+    };
+
+
+    return (
+        <AppBar className="app__header">
+                {mobileView ? displayMobile() : displayDesktop()}
+        </AppBar>
+    )
 }
 
 export default Navbar
